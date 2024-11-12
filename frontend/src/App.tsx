b0a5840dbf26f8c997092c0ec136a173b95c91f0
anthony-iheonye <iheonyeac@gmail.com>
2024-11-12 11:54:49 -0500

    frontend: Build a responsive layout.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,32 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav"
+               "aside"
+               "main"
+               "copyright"`,
+        lg: `"nav nav"
+             "aside main"
+              "copyright copyright"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="gold">
+        aside
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+      <GridItem area="copyright" bg="green">
+        Footer
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
