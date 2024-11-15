2dfafe0261086b276f4aab0e3c877bb3981fdfbd
anthony-iheonye <iheonyeac@gmail.com>
2024-11-15 02:09:40 -0500

    frontend: Build Image Card

---
+++ frontend/src/components/ImageCard.tsx
@@ -0,0 +1,22 @@
+import { Card, CardBody, Image, Text } from "@chakra-ui/react";
+import AugImage from "../entities/Image";
+import AugMask from "../entities/Mask";
+
+interface Props {
+  file: AugImage | AugMask;
+}
+
+const ImageCard = ({ file }: Props) => {
+  return (
+    <Card borderRadius={0}>
+      <Image src={file.url} />
+      <CardBody paddingTop={1} paddingBottom={1}>
+        <Text color="gray" fontSize="xs">
+          {file.name}
+        </Text>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default ImageCard;
