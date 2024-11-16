a69ca1c936f3e8c7fbc982813e50a2bb227c6741
anthony-iheonye <iheonyeac@gmail.com>
2024-11-15 19:34:19 -0500

    frontend: Build preview switch

---
+++ frontend/src/components/PreviewSwitch.tsx
@@ -0,0 +1,22 @@
+import { HStack, Switch, Text } from "@chakra-ui/react";
+import useAugConfigStore from "../store";
+
+const PreviewSwitch = () => {
+  const setPreviewSelection = useAugConfigStore(
+    (state) => state.setPreviewSelection
+  );
+  const previewSelection = useAugConfigStore((state) => state.previewSelection);
+
+  return (
+    <HStack>
+      <Switch
+        colorScheme="green"
+        isChecked={previewSelection === true}
+        onChange={() => setPreviewSelection(!previewSelection)}
+      />
+      <Text whiteSpace="nowrap">Preview selection</Text>
+    </HStack>
+  );
+};
+
+export default PreviewSwitch;
