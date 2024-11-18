cbd87ab68d08ca445d38227594b5de11afeaa660
anthony-iheonye <iheonyeac@gmail.com>
2024-11-18 09:32:07 -0500

    frontend: Refactor company logo

--- frontend/src/components/CompanyLogo.tsx
+++ frontend/src/components/CompanyLogo.tsx
@@ -1,12 +1,13 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
-import { Link } from "@chakra-ui/react";
+import { Box, Image, Link } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 
 const CompanyLogo = () => {
   return (
-    <Link>
-      <Image src={logo} boxSize="60px" objectFit="cover" />
-    </Link>
+    <Box width="auto" alignSelf="center">
+      <Link>
+        <Image src={logo} boxSize="60px" objectFit="cover" />
+      </Link>
+    </Box>
   );
 };
 
