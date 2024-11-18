08ec9fda5dbd3554d8ddc6b574d0e29bd65f18ff
anthony-iheonye <iheonyeac@gmail.com>
2024-11-17 22:36:26 -0500

    frontend: Modify the colormode switch

--- frontend/src/components/ColorModeSwitch.tsx
+++ frontend/src/components/ColorModeSwitch.tsx
@@ -1,17 +1,21 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { HStack, IconButton, useColorMode } from "@chakra-ui/react";
+import { FaMoon } from "react-icons/fa6";
+import { GoSun } from "react-icons/go";
 
 const ColorModeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <HStack>
-      <Switch
-        colorScheme="green"
-        size="sm"
-        isChecked={colorMode == "dark"}
-        onChange={toggleColorMode}
+      <IconButton
+        aria-label="Toggle color mode"
+        icon={colorMode === "dark" ? <GoSun /> : <FaMoon />}
+        onClick={toggleColorMode}
+        variant="ghost"
+        size="lg"
+        fontSize="1.5rem"
+        colorScheme={colorMode === "dark" ? "yellow" : "gray"}
       />
-      <Text>Dark Mode</Text>
     </HStack>
   );
 };
