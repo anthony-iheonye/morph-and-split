2805e71bb8b97f3be9740f5140b0007ba761f113
anthony-iheonye <iheonyeac@gmail.com>
2024-11-18 09:29:37 -0500

    frontend: Improve NavBar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,13 +1,35 @@
-import { HStack } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
+import AugmentationConfigIcon from "./AugConfigurationIcon";
+import AugmentIcon from "./AugmentIcon";
 import ColorModeSwitch from "./ColorModeSwitch";
 import CompanyLogo from "./CompanyLogo";
+import ImageMaskUploader from "./ImageMaskUploader";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between">
-      <CompanyLogo />
-      <ColorModeSwitch />
-    </HStack>
+    <Flex
+      direction={{ base: "row", md: "column" }}
+      height={{ md: "100vh" }}
+      padding="10px"
+    >
+      <Flex
+        direction={{ base: "row", md: "column" }}
+        gap={{ base: "4", md: 12 }}
+        flexGrow={1}
+      >
+        <CompanyLogo />
+        <ImageMaskUploader />
+        <AugmentationConfigIcon />
+        <AugmentIcon />
+      </Flex>
+
+      <Box
+        alignSelf={{ base: "flex-end", md: "center" }}
+        mb={{ base: 1, md: 4 }}
+      >
+        <ColorModeSwitch />
+      </Box>
+    </Flex>
   );
 };
 
