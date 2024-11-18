dd40954ac58f1688c175d7a156732e9f34c80f6e
anthony-iheonye <iheonyeac@gmail.com>
2024-11-17 19:30:32 -0500

    frontend: Create hook to get json file on Visual attribute of peas on each image.

---
+++ frontend/src/hooks/useVisualAttributesFile.ts
@@ -0,0 +1,35 @@
+import { ChangeEvent, useState } from "react";
+import { getFileExt } from "../fileUtils";
+import useAugConfigAndSetter from "./useAugConfigAndSetter";
+
+const useVisualAttributesFile = () => {
+  const [error, setError] = useState<string | null>(null);
+  const { augConfig, setAugConfig } = useAugConfigAndSetter();
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const curFiles = event.target.files;
+    const previousFile = augConfig.visualAttributesJSONFile?.name || "";
+
+    if (curFiles && getFileExt(curFiles[0]) !== "json") {
+      setError(`Files most be in JSON format. Selected ${curFiles[0].name}`);
+      return;
+    }
+
+    if (!curFiles || (curFiles.length == 0 && previousFile === "")) {
+      setError("No file on visual attribute was selected.");
+      return;
+    }
+
+    setError(null);
+    const result = Array.from(curFiles)[0];
+
+    setAugConfig("visualAttributesJSONFile", {
+      name: result.name,
+      url: URL.createObjectURL(result),
+    });
+  };
+
+  return { error, handleFileChange, augConfig };
+};
+
+export default useVisualAttributesFile;
