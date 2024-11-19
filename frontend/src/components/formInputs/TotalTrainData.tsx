78aaa63af2cd97eea9a8b7d04cc741342c94012c
anthony-iheonye <iheonyeac@gmail.com>
2024-11-19 03:50:57 -0500

    frontend: Update component for inputing the total traing image post-augment.

--- frontend/src/components/formInputs/TotalTrainData.tsx
+++ frontend/src/components/formInputs/TotalTrainData.tsx
@@ -1,8 +1,4 @@
 import {
-  FormControl,
-  FormLabel,
-  InputGroup,
-  InputLeftAddon,
   NumberDecrementStepper,
   NumberIncrementStepper,
   NumberInput,
@@ -15,28 +11,22 @@ const TotalTrainData = () => {
   const { augConfig, setAugConfig } = useAugConfigAndSetter();
 
   return (
-    <FormControl>
-      <FormLabel> Total training image after augmentation </FormLabel>
-      <InputGroup size="sm">
-        <InputLeftAddon width="5.5rem">Size</InputLeftAddon>
-        <NumberInput
-          defaultValue={1}
-          min={1}
-          max={1000000}
-          allowMouseWheel
-          value={augConfig.totalAugmentedImages}
-          onChange={(value) =>
-            setAugConfig("totalAugmentedImages", parseInt(value))
-          }
-        >
-          <NumberInputField />
-          <NumberInputStepper>
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-          </NumberInputStepper>
-        </NumberInput>
-      </InputGroup>
-    </FormControl>
+    <NumberInput
+      min={1}
+      max={100000}
+      allowMouseWheel
+      width="auto"
+      value={augConfig.totalAugmentedImages}
+      onChange={(value) =>
+        setAugConfig("totalAugmentedImages", parseInt(value))
+      }
+    >
+      <NumberInputField />
+      <NumberInputStepper>
+        <NumberIncrementStepper />
+        <NumberDecrementStepper />
+      </NumberInputStepper>
+    </NumberInput>
   );
 };
 
