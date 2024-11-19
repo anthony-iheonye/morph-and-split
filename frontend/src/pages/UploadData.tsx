25111aeb5ad3e1522f6d87ffdc3d5c72cfed5ee4
anthony-iheonye <iheonyeac@gmail.com>
2024-11-18 20:12:34 -0500

    frontend: Build UploadData page

---
+++ frontend/src/pages/UploadData.tsx
@@ -0,0 +1,31 @@
+import { Box, Grid, GridItem } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
+import UploadDataBar from "../components/UploadDataBar";
+
+const UploadData = () => {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"upload_bar"
+               "outlet"`,
+        md: `"upload_bar outlet"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        md: "200px 1fr",
+      }}
+    >
+      <GridItem area="upload_bar">
+        <UploadDataBar />
+      </GridItem>
+
+      <GridItem area="outlet">
+        <Box marginTop={8}>
+          <Outlet />
+        </Box>
+      </GridItem>
+    </Grid>
+  );
+};
+
+export default UploadData;
