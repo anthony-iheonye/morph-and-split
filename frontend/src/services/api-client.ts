b631b9ed1781339a1ee80fdf211ed6be2eccf726
anthony-iheonye <iheonyeac@gmail.com>
2024-11-23 18:47:26 -0500

    frontend: Update API client

--- frontend/src/services/api-client.ts
+++ frontend/src/services/api-client.ts
@@ -1,5 +1,28 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
-export default axios.create({
-  baseURL: "http://localhost:5000/augment",
+const axiosInstance = axios.create({
+  baseURL: "http://127.0.0.1:5000", // Backend base URL
 });
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  uploadFiles = (data: FormData, requestConfig?: AxiosRequestConfig) =>
+    axiosInstance
+      .post<T>(this.endpoint, data, requestConfig)
+      .then((res) => res.data);
+
+  fetchAugmentResults = (
+    id: string | number,
+    requestConfig?: AxiosRequestConfig
+  ) =>
+    axiosInstance
+      .get<T>(`${this.endpoint}/${id}`, requestConfig)
+      .then((res) => res.data);
+}
+
+export default APIClient;
