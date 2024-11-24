d71b366b59e7d9b9776a1c199a3931d1e03c3390
anthony-iheonye <iheonyeac@gmail.com>
2024-11-23 19:08:47 -0500

    frontend: Remove duplicate Link in CompanyLogo component.

--- frontend/src/components/CompanyLogo.tsx
+++ frontend/src/components/CompanyLogo.tsx
@@ -1,12 +1,10 @@
-import { Box, Image, Link } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 
 const CompanyLogo = () => {
   return (
     <Box width="auto" alignSelf="center">
-      <Link>
-        <Image src={logo} boxSize="60px" objectFit="cover" />
-      </Link>
+      <Image src={logo} boxSize="60px" objectFit="cover" />
     </Box>
   );
 };
