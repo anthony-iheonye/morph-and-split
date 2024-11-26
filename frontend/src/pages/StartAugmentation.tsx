bc60ac2c561471ebc477eaba52598ed83fa41355
anthony-iheonye <iheonyeac@gmail.com>
2024-11-26 17:09:12 -0500

    frontend: Update StatrtAugmentation page - remove progressing wheel.

--- frontend/src/pages/StartAugmentation.tsx
+++ frontend/src/pages/StartAugmentation.tsx
@@ -1,14 +1,6 @@
-import {
-  Button,
-  CircularProgress,
-  CircularProgressLabel,
-  HStack,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { FaPlay } from "react-icons/fa6";
+import { HStack, Text, VStack } from "@chakra-ui/react";
 import { TbNumbers } from "react-icons/tb";
+import Augment from "../components/Augment";
 import BoundingBox from "../components/BoundingBox";
 import IconHeadingDescriptionCombo from "../components/IconHeadingDescriptionCombo";
 import PageTitle from "../components/PageTitle";
@@ -17,38 +9,6 @@ import TrainStartIndex from "../components/TrainStartIndex";
 import ValStartIndex from "../components/ValStartIndex";
 
 const StartAugmentation = () => {
-  const [progress, setProgress] = useState(0);
-  const [isRunning, setIsRunning] = useState(false); // Track if augmentation is running
-
-  useEffect(() => {
-    let interval: NodeJS.Timeout;
-
-    if (isRunning) {
-      interval = setInterval(() => {
-        setProgress((prev) => {
-          const nextProgress = prev + 1;
-          if (nextProgress >= 100) {
-            clearInterval(interval); // Stop when progress reaches 100
-            setIsRunning(false); // Reset the running state
-            return 100;
-          }
-          return nextProgress;
-        });
-      }, 200);
-    }
-
-    return () => clearInterval(interval); // Cleanup on unmount or when isRunning changes
-  }, [isRunning]);
-
-  const handleToggle = () => {
-    if (isRunning) {
-      setIsRunning(false); // Stop the progress
-      setProgress(0); // Reset progress
-    } else {
-      setProgress(0); // Reset progress
-      setIsRunning(true); // Start the progress
-    }
-  };
   return (
     <>
       <PageTitle title="Start Augmentation" />
@@ -89,42 +49,7 @@ const StartAugmentation = () => {
       </BoundingBox>
 
       {/*Start Augmentation */}
-      <BoundingBox
-        maxWidth={"fit-content"}
-        justify={"center"}
-        padding={3}
-        borderRadius={8}
-      >
-        <Button
-          type="submit"
-          height="auto"
-          padding="4"
-          justifySelf="center"
-          _hover={{ bg: "teal" }}
-          onClick={handleToggle}
-        >
-          <IconHeadingDescriptionCombo
-            icon={FaPlay}
-            fontSize={30}
-            title={isRunning ? "Cancel Augmentation" : "Run Augmentation"}
-            description={
-              isRunning
-                ? "Stop the augmentation process."
-                : "Augment the selected images and segmentation masks."
-            }
-          />
-          {isRunning && ( // Show progress bar only when running
-            <CircularProgress
-              value={progress}
-              size="120px"
-              color="blue.400"
-              mt={4}
-            >
-              <CircularProgressLabel>{progress}%</CircularProgressLabel>
-            </CircularProgress>
-          )}
-        </Button>
-      </BoundingBox>
+      <Augment />
     </>
   );
 };
