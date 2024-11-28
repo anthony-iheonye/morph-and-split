c8dab142bb82ffd23bd48baf219fb8b07711fe98
anthony-iheonye <iheonyeac@gmail.com>
2024-11-28 13:36:55 -0500

    frontend: Add downloadFile method to API client

--- frontend/src/services/api-client.ts
+++ frontend/src/services/api-client.ts
@@ -16,13 +16,42 @@ class APIClient<T> {
       .post<T>(this.endpoint, data, requestConfig)
       .then((res) => res.data);
 
-  fetchAugmentResults = (
+  fetchAugmentedResults = (
     id: string | number,
     requestConfig?: AxiosRequestConfig
   ) =>
     axiosInstance
       .get<T>(`${this.endpoint}/${id}`, requestConfig)
       .then((res) => res.data);
+
+  downloadFile = async (
+    filename: string,
+    requestConfig?: AxiosRequestConfig
+  ) => {
+    // send get request download file from server
+    const response = await axiosInstance.get(
+      `${this.endpoint}/${encodeURIComponent(filename)}`,
+      { responseType: "blob", ...requestConfig }
+    );
+
+    // handle file download
+    if (response) {
+      // wrap the file content in a Blob, so its treated a downloadable file
+      const blob = new Blob([response.data]);
+      // create temporary url pointing to the blob
+      const url = window.URL.createObjectURL(blob);
+      // Create a link element
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = filename;
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+      window.URL.revokeObjectURL(url);
+    } else {
+      console.error("Failed to dowload file.");
+    }
+  };
 }
 
 export default APIClient;
