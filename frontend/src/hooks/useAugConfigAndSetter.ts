12d72d9fed19418ff11110b8913d095e7351267e
anthony-iheonye <iheonyeac@gmail.com>
2024-11-27 22:30:24 -0500

    frontend: Update useAugConfigAndSetter hook.

--- frontend/src/hooks/useAugConfigAndSetter.ts
+++ frontend/src/hooks/useAugConfigAndSetter.ts
@@ -1,11 +1,14 @@
 import useAugConfigStore from "../store/augConfigStore";
 
 const useAugConfigAndSetter = () => {
-  const { augConfig, setAugConfig } = useAugConfigStore((store) => ({
-    augConfig: store.augConfig,
-    setAugConfig: store.setAugConfig,
-  }));
-  return { augConfig, setAugConfig };
+  const { augConfig, setAugConfig, resetAugConfig } = useAugConfigStore(
+    (store) => ({
+      augConfig: store.augConfig,
+      setAugConfig: store.setAugConfig,
+      resetAugConfig: store.resetAugConfig,
+    })
+  );
+  return { augConfig, setAugConfig, resetAugConfig };
 };
 
 export default useAugConfigAndSetter;
