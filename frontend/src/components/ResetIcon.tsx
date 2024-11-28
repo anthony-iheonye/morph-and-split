6122c59c18498287f22eeddc20a42ed2ab18d95e
anthony-iheonye <iheonyeac@gmail.com>
2024-11-27 22:28:38 -0500

    frontend: Update ResetIcon

--- frontend/src/components/ResetIcon.tsx
+++ frontend/src/components/ResetIcon.tsx
@@ -1,11 +1,16 @@
 import { Box, IconButton, Tooltip } from "@chakra-ui/react";
 import { VscDebugRestart } from "react-icons/vsc";
+import useAugConfigAndSetter from "../hooks/useAugConfigAndSetter";
 import useNavIconColor from "../hooks/useNavIconColor";
-import useAugConfigStore from "../store/augConfigStore";
 
 const ResetIcon = () => {
   const backgroundColor = useNavIconColor();
-  const resetAugConfig = useAugConfigStore((state) => state.resetAugConfig);
+  const { augConfig, setAugConfig, resetAugConfig } = useAugConfigAndSetter();
+
+  const handleReset = (key: keyof typeof augConfig) => {
+    resetAugConfig();
+    setAugConfig(key, !augConfig[key]);
+  };
 
   return (
     <Box width="auto" alignSelf="center">
@@ -17,7 +22,7 @@ const ResetIcon = () => {
           size="lg"
           fontSize="1.5rem"
           colorScheme={backgroundColor}
-          onClick={() => resetAugConfig()}
+          onClick={() => handleReset("reset")}
         />
       </Tooltip>
     </Box>
