3a4149f78be43584e8034b556aa3f8c1541a22b1
anthony-iheonye <iheonyeac@gmail.com>
2024-11-30 00:28:49 -0500

    frontend: update viewState to view augConfig and nav state.

--- frontend/src/components/ViewState.tsx
+++ frontend/src/components/ViewState.tsx
@@ -1,14 +1,25 @@
 import { Switch } from "@chakra-ui/react";
 import useAugConfigAndSetter from "../hooks/useAugConfigAndSetter";
+import useNavStore from "../store/navStore";
 
 const ViewState = () => {
   const { augConfig } = useAugConfigAndSetter();
+  const { activeParent, activeSubParent } = useNavStore((store) => ({
+    activeParent: store.activeParent,
+    activeSubParent: store.activeSubParent,
+  }));
+
   return (
     <Switch
       id="augmentValSet"
       colorScheme="teal"
-      // isChecked={augConfig.augmentValData}
-      onChange={() => console.log(augConfig)}
+      onChange={() =>
+        console.log(
+          augConfig,
+          { activaeParent: activeParent },
+          { activeSubparent: activeSubParent }
+        )
+      }
     />
   );
 };
