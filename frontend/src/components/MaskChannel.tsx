0f8b80eb921f4f2ae70155d9e67ad6ae6a2752e8
anthony-iheonye <iheonyeac@gmail.com>
2024-11-29 19:26:42 -0500

    frontend: Update  MaskChannel component

--- frontend/src/components/MaskChannel.tsx
+++ frontend/src/components/MaskChannel.tsx
@@ -1,25 +1,27 @@
-import { InputGroup, Select } from "@chakra-ui/react";
+import { Select } from "@chakra-ui/react";
 import useAugConfigAndSetter from "../hooks/useAugConfigAndSetter";
 
 const MaskChannel = () => {
   const { augConfig, setAugConfig } = useAugConfigAndSetter();
 
   return (
-    <InputGroup size="sm" width="auto" borderRadius="10px">
-      <Select
-        value={augConfig.imageMaskChannels?.maskChannels}
-        onChange={(e) =>
-          setAugConfig("imageMaskChannels", {
-            ...augConfig.imageMaskChannels,
-            imgChannels: augConfig.imageMaskChannels!.imgChannels,
-            maskChannels: parseInt(e.target.value),
-          })
-        }
-      >
-        <option value={1}>1 - Binary/Grayscale</option>
-        <option value={3}>3 - RGB</option>
-      </Select>
-    </InputGroup>
+    <Select
+      // size="sm"
+      width="auto"
+      // borderRadius="10px"
+
+      value={augConfig.imageMaskChannels?.maskChannels}
+      onChange={(e) =>
+        setAugConfig("imageMaskChannels", {
+          ...augConfig.imageMaskChannels,
+          imgChannels: augConfig.imageMaskChannels!.imgChannels,
+          maskChannels: parseInt(e.target.value),
+        })
+      }
+    >
+      <option value={1}>1 - Binary/Grayscale</option>
+      <option value={3}>3 - RGB</option>
+    </Select>
   );
 };
 
