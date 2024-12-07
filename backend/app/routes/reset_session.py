3ec957b6ae6a8dd073068a8cb6679320b431f724
anthony-iheonye <iheonyeac@gmail.com>
2024-12-06 19:49:15 -0500

    backend: Update reset_session.py - set the response to be a dict containing success and/error message.

---
+++ backend/app/routes/reset_session.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, jsonify
+
+from app.utils import create_project_directories
+
+# Blueprint definition
+reset_session = Blueprint('reset_session', __name__)
+
+@reset_session.route('/reset_session', methods=['POST'])
+def reset_project_session():
+    try:
+        # Delete all data by creating empty project folders
+        create_project_directories(return_dir=False, overwrite_if_existing=True)
+
+        return jsonify({'success': True}), 201
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)}), 500
