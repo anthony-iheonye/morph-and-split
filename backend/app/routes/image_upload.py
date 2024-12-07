4061e46fac6339d0ab64c26eb321d45e4f3aea56
anthony-iheonye <iheonyeac@gmail.com>
2024-12-06 19:57:03 -0500

    backend: Improve the format of the json response to include success and error keys.

--- backend/app/routes/image_upload.py
+++ backend/app/routes/image_upload.py
@@ -1,22 +1,41 @@
+import gc
+import os
+
 from flask import Blueprint, request, jsonify
 from werkzeug.utils import secure_filename
-import os
+
+from app.services import resize_image
 from app.utils import directory_store
 
 image_upload = Blueprint('image_upload', __name__)
 IMAGE_DIR = directory_store.image_dir
+RESIZED_IMAGE_DIR = directory_store.resized_image_dir
 
 @image_upload.route('/upload/images', methods=['POST'])
 def upload_images():
-    images = request.files.getlist('images')
+    try:
+        images = request.files.getlist('images')
 
-    if not images:
-        return jsonify({'message': 'No images uploaded'}), 400
+        if not images:
+            return jsonify({'message': 'No images uploaded'}), 400
 
-    for img in images:
-        print(img.filename)
-        filename = secure_filename(img.filename)
-        filepath = os.path.join(IMAGE_DIR, filename)
-        img.save(filepath)
+        for img in images:
+            print(img.filename)
+            filename = secure_filename(img.filename)
+            filepath = os.path.join(IMAGE_DIR, filename)
+            img.save(filepath)
 
-    return jsonify({'message': 'Images uploaded successfully.'}), 200
+            resized_file_path = os.path.join(RESIZED_IMAGE_DIR, filename)
+
+            resize_image(original_image_path=filepath,
+                         resized_image_path=resized_file_path,
+                         )
+        return jsonify({'success': True}), 200
+
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)}), 400
+
+    finally:
+        del images
+        gc.collect()
+
--- backend/app/routes/mask_upload.py
+++ backend/app/routes/mask_upload.py
@@ -1,22 +1,41 @@
+import gc
+import os
+
 from flask import Blueprint, request, jsonify
 from werkzeug.utils import secure_filename
-import os
+
+from app.services import resize_image
 from app.utils import directory_store
 
 mask_upload = Blueprint('mask_upload', __name__)
 MASK_DIR = directory_store.mask_dir
+RESIZED_MASK_DIR = directory_store.resized_mask_dir
+
 
 @mask_upload.route('/upload/masks', methods=['POST'])
 def upload_masks():
-    masks = request.files.getlist('masks')
+    try:
+        masks = request.files.getlist('masks')
 
-    if not masks:
-        return jsonify({'message': 'No mask uploaded'}), 400
+        if not masks:
+            return jsonify({'message': 'No mask uploaded'}), 400
 
-    for mask in masks:
-        print(mask.filename)
-        filename = secure_filename(mask.filename)
-        filepath = os.path.join(MASK_DIR, filename)
-        mask.save(filepath)
+        for mask in masks:
+            print(mask.filename)
+            filename = secure_filename(mask.filename)
+            filepath = os.path.join(MASK_DIR, filename)
+            mask.save(filepath)
 
-    return jsonify({'message': 'Masks uploaded successfully.'}), 200
+            resized_file_path = os.path.join(RESIZED_MASK_DIR, filename)
+
+            resize_image(original_image_path=filepath,
+                         resized_image_path=resized_file_path,
+                         )
+        return jsonify({'success': True}), 200
+
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)}), 400
+
+    finally:
+        del masks
+        gc.collect()
