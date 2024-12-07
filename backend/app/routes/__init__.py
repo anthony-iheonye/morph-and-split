5d062cc910549222cc2d44bbca226572f0bc745f
anthony-iheonye <iheonyeac@gmail.com>
2024-12-06 19:57:57 -0500

    backend: Build route for serving resized version of the uploaded images and masks.

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -4,3 +4,6 @@ from .augment import augment
 from .mask_upload import mask_upload
 from .file_download import file_download
 from .image_mask_metadata import image_mask_metadata
+from .uploaded_images_name import uploaded_image_names
+from .uploaded_masks_name import uploaded_mask_names
+from .reset_session import reset_session
--- backend/app/routes/image_mask_metadata.py
+++ backend/app/routes/image_mask_metadata.py
@@ -1,24 +1,47 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, send_from_directory, url_for
 
-from app.utils import directory_store, get_sorted_filepaths
+from app.utils import directory_store, get_sorted_filenames
 
+# Blueprint definition
 image_mask_metadata = Blueprint('image_mask_metadata', __name__)
-IMAGE_DIR = directory_store.image_dir
-MASK_DIR = directory_store.mask_dir
+
+# directory definition
+IMAGE_DIR = directory_store.resized_image_dir
+MASK_DIR = directory_store.resized_mask_dir
+
+
+# Route to serve image files
+@image_mask_metadata.route('/images/<filename>')
+def serve_image(filename):
+    return send_from_directory(IMAGE_DIR, filename)
+
+
+# Route to serve mask files
+@image_mask_metadata.route('/masks/<filename>')
+def serve_mask(filename):
+    return send_from_directory(MASK_DIR, filename)
+
 
 @image_mask_metadata.route('/metadata/image_mask', methods=['GET'])
 def get_image_mask_metadata():
     try:
-        metadata = []
+        # Get sorted filenames for images and masks
+        image_files = get_sorted_filenames(IMAGE_DIR)
+        mask_files = get_sorted_filenames(MASK_DIR)
 
-        for image_name, mask_name in zip([get_sorted_filepaths(IMAGE_DIR),
-                                          get_sorted_filepaths(MASK_DIR)]):
-            metadata.append({'imageName': image_name,
-                             'maskName': mask_name,
-                             'imageUrl': f"{IMAGE_DIR}/{image_name}",
-                             'maskUrl': f"{MASK_DIR}/{mask_name}"
+        # Ensure the number of images and mask align
+        if len(image_files) != len(mask_files):
+            return jsonify({'error': "Mismatch between number of images and masks."}), 400
+
+        metadata = []
+        for image_name, mask_name in zip(image_files, mask_files):
+            image_path = url_for('image_mask_metadata.serve_image', filename=image_name, _external=True)
+            mask_path = url_for('image_mask_metadata.serve_mask', filename=mask_name, _external=True)
+
+            metadata.append({'image': {'name': image_name, 'url': image_path},
+                             'mask': {'name': mask_name, 'url': mask_path}
                              })
 
-        return jsonify(metadata), 200
+        return jsonify({'count': len(metadata), 'results': metadata}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
