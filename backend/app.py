59fcbd6fea7bf5e17f977333943a3eaecf0d4ad0
anthony-iheonye <iheonyeac@gmail.com>
2024-12-06 20:03:46 -0500

    backend: Refactor app.py and include the blueprint to routes for resetting session and serving names of uploaded images and masks.

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
 
-from app.routes import image_upload, mask_upload, file_download, augment, image_mask_metadata
+from app.routes import image_upload, mask_upload, file_download, augment, image_mask_metadata, uploaded_image_names, \
+    uploaded_mask_names, reset_session
 from app.utils import create_project_directories
 
 # Flask app setup
@@ -9,8 +10,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Create project directories
-directories = create_project_directories()
-IMAGE_DIR, MASK_DIR, AUGMENTED_DIR, TRAIN_DIR, VAL_DIR, TEST_DIR, VISUAL_ATTRIBUTES= directories.values()
+directories = create_project_directories(overwrite_if_existing=False)
 
 for directory in directories.values():
     print(directory)
@@ -20,6 +20,9 @@ for directory in directories.values():
 app.register_blueprint(image_upload)
 app.register_blueprint(mask_upload)
 app.register_blueprint(image_mask_metadata)
+app.register_blueprint(uploaded_image_names)
+app.register_blueprint(uploaded_mask_names)
+app.register_blueprint(reset_session)
 app.register_blueprint(file_download)
 app.register_blueprint(augment)
 
