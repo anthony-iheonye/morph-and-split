f74fc6456f21dd97548bf138bf9047637db598d1
anthony-iheonye <iheonyeac@gmail.com>
2024-12-13 12:01:31 -0500

    frontend: Build inidividual PreviewGrid components for displaying train, val and test data.

---
+++ frontend/src/components/PreviewGridVal.tsx
@@ -0,0 +1,32 @@
+import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import useValidationSet from "../hooks/useValidationSet";
+import useAugConfigStore from "../store/augConfigStore";
+import PreviewCard from "./PreviewCard";
+import PreviewContainer from "./PreviewContainer";
+
+const PreviewGridVal = () => {
+  const { previewAugmentedResult, previewedSet } = useAugConfigStore(
+    (state) => ({
+      previewAugmentedResult: state.previewAugmentedResult,
+      previewedSet: state.augConfig.previewedSet,
+    })
+  );
+
+  const { data, error, isLoading } = useValidationSet();
+
+  if (!previewAugmentedResult || previewedSet != "val") return null;
+  if (isLoading) return <Spinner />;
+  if (error) return <Text color="red.500">Failed to load validation set.</Text>;
+
+  return (
+    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={6} padding={"10px"}>
+      {data?.results.map(({ image, mask }) => (
+        <PreviewContainer key={image.name}>
+          <PreviewCard image={image} mask={mask} />
+        </PreviewContainer>
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default PreviewGridVal;
