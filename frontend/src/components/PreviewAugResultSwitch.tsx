dada7100f985515086ed729f17a05d5614971075
anthony-iheonye <iheonyeac@gmail.com>
2024-12-13 12:02:36 -0500

    frontend: Refactor switch fro previewing augmented data.

--- frontend/src/components/PreviewAugResultSwitch.tsx
+++ frontend/src/components/PreviewAugResultSwitch.tsx
@@ -2,18 +2,22 @@ import { Switch } from "@chakra-ui/react";
 import useAugConfigStore from "../store/augConfigStore";
 
 const PreviewAugResultSwitch = () => {
-  const { setPreviewAugmentedResult } = useAugConfigStore((state) => ({
-    setPreviewAugmentedResult: state.setPreviewAugmentedResult,
-  }));
-  const previewAugmentedResult = useAugConfigStore(
-    (state) => state.previewAugmentedResult
-  );
+  const { previewAugmentedResult, setPreviewAugmentedResult } =
+    useAugConfigStore((state) => ({
+      previewAugmentedResult: state.previewAugmentedResult,
+      setPreviewAugmentedResult: state.setPreviewAugmentedResult,
+    }));
+
+  const toggleSwitch = async () => {
+    const nextPreviewState = !previewAugmentedResult; // Calculate the next state
+    setPreviewAugmentedResult(nextPreviewState); // Update the state
+  };
 
   return (
     <Switch
       colorScheme="green"
       isChecked={previewAugmentedResult === true}
-      onChange={() => setPreviewAugmentedResult(!previewAugmentedResult)}
+      onChange={toggleSwitch}
     />
   );
 };
