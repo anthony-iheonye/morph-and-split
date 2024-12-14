096943f78d3e4896099d27b22d018a58193d9735
anthony-iheonye <iheonyeac@gmail.com>
2024-12-14 12:53:59 -0500

    frontend: Create index.ts file for folder housing main nav bar items.

---
+++ frontend/src/components/navigation/mainNavBarItems/index.ts
@@ -0,0 +1,6 @@
+export { default as AugmentationConfigIcon } from "./AugConfigurationIcon";
+export { default as AugmentIcon } from "./AugmentIcon";
+export { default as ColorModeSwitch } from "./ColorModeSwitch";
+export { default as ImageMaskUploader } from "./ImageMaskUploader";
+export { default as ResetIcon } from "./ResetIcon";
+export { default as ViewState } from "./ViewState";
