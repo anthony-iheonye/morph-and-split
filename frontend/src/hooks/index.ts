8aaa334aa0316e0c4cb0118a2923a4ebfb381f5e
anthony-iheonye <iheonyeac@gmail.com>
2024-12-14 03:20:56 -0500

    frontend: Create index.ts file for hooks folder

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,17 @@
+export { default as useActiveNavColor } from "./useActiveNavColor";
+export { default as useActiveParent } from "./useActiveParent";
+export { default as useActiveSubParent } from "./useActiveSubParent";
+export { default as useAugConfigAndSetter } from "./useAugConfigAndSetter";
+export { default as useBackendResponse } from "./useBackendResponse";
+export { default as useBoundingBoxColor } from "./useBoundingBoxColor";
+export { default as useFileSelector } from "./useFileSelector";
+export { default as useFileUploader } from "./useFileUploader";
+export { default as useNavBarColor } from "./useNavBarColor";
+export { default as useNavIconColor } from "./useNavIconColor";
+export { default as useSplitSet } from "./useSplitSet";
+export { default as useTrainingSet } from "./useTrainingSet";
+export { default as useTestSet } from "./useTestSet";
+export { default as useUploadedImageMask } from "./useUploadedImageMask";
+export { default as useUploadedImageNames } from "./useUploadedImageNames";
+export { default as useUploadedMaskNames } from "./useUploadedMaskNames";
+export { default as useVisualAttributesFile } from "./useVisualAttributesFile";
