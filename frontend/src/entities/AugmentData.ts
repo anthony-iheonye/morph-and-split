f5ce9c8fc1f7602c096833e8bf06cfdb671e9e53
anthony-iheonye <iheonyeac@gmail.com>
2024-12-20 12:45:02 -0500

    frontend: delete unused scripts

--- frontend/src/entities/AugmentData.ts
+++
@@ -1,7 +0,0 @@
-interface AugmentedData {
-  filename: string;
-  path: string;
-  data: BinaryData;
-}
-
-export default AugmentedData;
--- frontend/src/hooks/useNavBarColor.ts
+++
@@ -1,9 +0,0 @@
-import { useColorMode } from "@chakra-ui/react";
-
-const useNavBarColor = () => {
-  const { colorMode } = useColorMode();
-
-  return colorMode === "dark" ? "gray.700" : "gray.100";
-};
-
-export default useNavBarColor;
