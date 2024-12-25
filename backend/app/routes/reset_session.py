a33fece3b4a427c07d24ec1ca02a073ec585850c
anthony-iheonye <iheonyeac@gmail.com>
2024-12-25 02:29:12 -0500

    backend: re-created directories for resized augmented result, after resetting the session.

--- backend/app/routes/reset_session.py
+++ backend/app/routes/reset_session.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 
-from app.utils import create_project_directories
+from app.utils import create_project_directories, create_resized_augmentation_directories
 
 # Blueprint definition
 reset_session = Blueprint('reset_session', __name__)
@@ -10,6 +10,7 @@ def reset_project_session():
     try:
         # Delete all data by creating empty project folders
         create_project_directories(return_dir=False, overwrite_if_existing=True)
+        create_resized_augmentation_directories(return_dir=False, overwrite_if_existing=True)
 
         return jsonify({'success': True}), 201
     except Exception as e:
