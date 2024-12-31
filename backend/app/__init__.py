b0b86ca9bb4986e7166acfa357c38c71c55dba5d
anthony-iheonye <iheonyeac@gmail.com>
2024-12-31 16:21:56 -0500

    backend: Update app.py: Set CORS configuration and add signed_upload_url blueprint.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 
 from app import aug_config
 from app.routes import image_upload, mask_upload, file_download, augment, image_mask_metadata, uploaded_image_names, \
-    uploaded_mask_names, reset_session
-from app.utils import create_project_directories
-
+    uploaded_mask_names, reset_session, signed_upload_url
+from app.utils import create_project_directories, create_google_cloud_storage_bucket, current_directory, set_cors_configuration
 
 def create_app():
     # Flask app setup
@@ -14,8 +14,15 @@ def create_app():
 
     # Create project directories
     create_project_directories(return_dir=False, overwrite_if_existing=False)
+    set_cors_configuration(bucket_name='morph-and-split-assets')
+
+    # Create Google Cloud Storage
+
+    # create_google_cloud_storage_bucket(bucket_name='morph-and-split-assets',
+    #                                    project='morph-and-split')
 
     # Register blueprints
+    app.register_blueprint(signed_upload_url)
     app.register_blueprint(image_upload)
     app.register_blueprint(mask_upload)
     app.register_blueprint(image_mask_metadata)
