bd4e10fa8b24e001267c32f0f65209e08285233d
anthony-iheonye <iheonyeac@gmail.com>
2025-01-01 22:50:46 -0500

    frontend: Update GCS folder paths

---
+++ frontend/src/store/googleCloudStore.ts
@@ -0,0 +1,14 @@
+const bucketFolders = {
+  images: "images",
+  masks: "masks",
+  resized_images: "resized_images",
+  resized_masks: "resized_masks",
+  train_images: "augmented/train/images",
+  train_masks: "augmented/train/masks",
+  val_images: "augmented/val/images",
+  val_masks: "augmented/val/masks",
+  test_images: "augmented/test/images",
+  test_masks: "augmented/test/masks",
+};
+
+export default bucketFolders;
