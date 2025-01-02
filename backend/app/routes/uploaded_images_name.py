272d269b75734fea42f667974e128b94695a1fa6
anthony-iheonye <iheonyeac@gmail.com>
2025-01-02 01:52:57 -0500

    backend: Create function for fetching names of uploaded images and masks.

--- backend/app/routes/uploaded_images_name.py
+++ backend/app/routes/uploaded_images_name.py
@@ -1,15 +1,16 @@
-import os
 from flask import Blueprint, jsonify
-from app.utils import directory_store, get_sorted_filenames
 
+from app.config import google_cloud_config
+from app.utils import list_files_in_bucket_directory
 
 uploaded_image_names = Blueprint('uploaded_image_names', __name__)
-IMAGE_DIR = directory_store.image_dir
 
 @uploaded_image_names.route('/upload/image_names', methods=['GET'])
 def get_uploaded_image_names():
     try:
-        image_files = get_sorted_filenames(IMAGE_DIR)
+        image_files = list_files_in_bucket_directory(bucket_name=google_cloud_config.bucket_name,
+                                                     directory_path=f"{google_cloud_config.image_dir}/")
+
         return jsonify({'success': True, 'count': len(image_files), 'results': image_files })
     except Exception as e:
         return jsonify({'error': str(e)})
--- backend/app/routes/uploaded_masks_name.py
+++ backend/app/routes/uploaded_masks_name.py
@@ -1,15 +1,16 @@
-import os
 from flask import Blueprint, jsonify
-from app.utils import directory_store, get_sorted_filenames
 
+from app.config import google_cloud_config
+from app.utils import list_files_in_bucket_directory
 
 uploaded_mask_names = Blueprint('uploaded_mask_names', __name__)
-MASK_DIR = directory_store.mask_dir
 
 @uploaded_mask_names.route('/upload/mask_names', methods=['GET'])
 def get_uploaded_image_names():
     try:
-        mask_files = get_sorted_filenames(MASK_DIR)
+        mask_files = list_files_in_bucket_directory(bucket_name=google_cloud_config.bucket_name,
+                                                    directory_path=f"{google_cloud_config.mask_dir}/")
+
         return jsonify({'count': len(mask_files), 'results': mask_files})
     except Exception as e:
         return jsonify({'error': str(e)})
