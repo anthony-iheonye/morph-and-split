37c1e0844cfb872b5d1af40d1ded4f8869206228
anthony-iheonye <iheonyeac@gmail.com>
2025-01-02 01:50:13 -0500

    backend: Write functions for listing, for downloading or for deleting files within a folder in a gcs bucket.

--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -8,4 +8,7 @@ from .directory_file_management import (create_google_cloud_storage_bucket,
 
 from .directory_file_management import (create_project_directories,
                                         get_sorted_filepaths,
-                                        create_resized_augmentation_directories, current_directory)
+                                        create_resized_augmentation_directories,
+                                        list_files_in_bucket_directory,
+                                        delete_files_in_google_cloud_storage_bucket,
+                                        download_files_from_gcs_folder)
--- backend/app/utils/directory_file_management.py
+++ backend/app/utils/directory_file_management.py
@@ -6,6 +6,7 @@ from typing import Union
 import attr
 from google.cloud import storage
 from google.cloud.exceptions import NotFound
+import subprocess
 
 current_dir = os.path.dirname(__file__)
 base_dir = os.path.abspath(os.path.join(current_dir, '../..'))
@@ -253,6 +254,71 @@ def create_google_cloud_storage_bucket(bucket_name: str,
         print(f"An error occurred: {e}")
         return None
 
+def delete_files_in_google_cloud_storage_bucket(bucket_name: str,
+                                                project: str = None,
+                                                location: str = 'us-south1',
+                                                storage_class='STANDARD',
+                                                enable_uniform_bucket_level_access: bool = True,
+                                                cors: list = None,
+                                                directories: list = None
+                                                ) -> Union[storage.Bucket, Exception, None]:
+    """
+    Delete files in the Google Cloud Bucket folders.
+
+    :param directories:
+    :param cors:
+    :param bucket_name: The bucket name
+    :param project: (str) The project under which the bucket is to be created. If not passed, uses the project set on the client.
+    :param location: (str) Location where the bucket is stored
+    :param storage_class: (str) The storage class. Can either be STANDARD (standard storage),
+        `NEARLINE` (nearline storage), `COLDLINE` (coldline storage) or `ARCHIVE` (archive storage).
+        View detailed description of each storage class visit: https://cloud.google.com/storage/docs/storage-classes#descriptions.
+    :param enable_uniform_bucket_level_access:
+    :return: The GCS bucket
+    """
+
+    try:
+        # Initialize the storage client
+        storage_client = storage.Client()
+
+        # Check if bucket already exists
+        try:
+            bucket = storage_client.get_bucket(bucket_name)
+            print(f"Bucket {bucket_name} already exists. Updating configuration...")
+        except NotFound:
+            print(f"Bucket {bucket_name} does not exist. Creating bucket...")
+            # Instantiate a bucket object to be owned by the 'storage_client'.
+            bucket = storage_client.bucket(bucket_name)
+            bucket = storage_client.create_bucket(bucket, location=location, project=project)
+
+        if directories:
+            for directory in directories:
+                # Add a trailing '/' to signify a folder and create an empty blob
+                blob = bucket.blob(f"{directory}/")
+                blob.upload_from_string("")
+
+        # Update the bucket's storage_class
+        if bucket.storage_class != storage_class:
+            bucket.storage_class = storage_class
+
+        # Update uniform bucket-level access status
+        current_access_setting = bucket.iam_configuration.uniform_bucket_level_access_enabled
+        if current_access_setting != enable_uniform_bucket_level_access:
+            bucket.iam_configuration.uniform_bucket_level_access_enabled = enable_uniform_bucket_level_access
+
+        # Set the CORS
+        bucket.cors = cors
+
+        # Save changes to the bucket
+        bucket.patch()
+        print(f"bucket {bucket_name} is now configured.")
+
+        return bucket
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
 
 def delete_google_cloud_storage_bucket(bucket_name: str):
     """
@@ -270,22 +336,22 @@ def delete_google_cloud_storage_bucket(bucket_name: str):
         print(f"Bucket {bucket_name} does not exist.")
 
 
-def create_google_cloud_storage_directories(bucket_name: str, directories: list):
+def list_files_in_bucket_directory(bucket_name: str, directory_path:str) -> list:
     """
-    Create directories for within a Google Cloud Storage bucket.
+    List the names of specific files within a directory in a Google Cloud Storage bucket.
 
-    :param bucket_name: (str) The name of the GCS bucket.
-    :param directories:  A list of directory paths to create within the GCS bucket.
-    :return:
+    :param bucket_name: (str) The bucket name
+    :param directory_path: (str) Path to the directory whose files should be listed.
+    :return: List of file names.
     """
-    # Create a storage client
+
     storage_client = storage.Client()
 
-    # Instantiate a bucket object to be owned by `storage_client`.
-    bucket = storage_client.bucket(bucket_name=bucket_name)
-
-    for directory in directories:
-        blob = bucket.blob(f"{directory}/")
+    # list all blobs (files) in teh specific directory
+    blobs = storage_client.list_blobs(bucket_or_name=bucket_name,
+                                      prefix=directory_path,
+                                      delimiter='/')
+    return sort_filenames([blob.name.split('/')[-1] for blob in blobs if not blob.name.endswith("/")])
 
 
 def upload_file_to_gcs_bucket(bucket_name:str, source_file_name:str, destination_blob_name:str):
@@ -309,6 +375,33 @@ def upload_file_to_gcs_bucket(bucket_name:str, source_file_name:str, destination
     # upload file to blob
     blob.upload_from_filename(source_file_name)
     print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+
+
+def download_files_from_gcs_folder(bucket_name: str, source_folder_path: str, destination_folder_path: str):
+    """
+    Copies files/folder from a Google Cloud Storage bucket to a local directory
+
+    :param bucket_name: (str) The name of the GCS bucket.
+    :param source_folder_path: (str) Path to the file/folder to be downloaded.
+    :param destination_folder_path: (str) Path to the local directory.
+    """
+    try:
+        source_directory = source_folder_path[:-2] if source_folder_path.endswith("/") else source_folder_path
+        gcloud_command = ["gcloud",
+                          "storage",
+                          "cp",
+                          f"gs://{bucket_name}/{source_directory}/*",
+                          destination_folder_path
+                          ]
+
+        result = subprocess.run(gcloud_command, check=True, capture_output=True)
+
+        print(f"File downloaded successfully!\n"
+              f"{result.stdout.decode('utf-8')}\n")
+
+    except subprocess.CalledProcessError as e:
+        print("Error while copying file from Google Cloud Storage: ", e.stderr)
+        return e.stderr
 
 
 def current_directory(file_path=None):
@@ -352,3 +445,4 @@ def get_sorted_filenames(images_dir):
 
     # sort the file paths in ascending order
     return sort_filenames(image_file_list)
+
