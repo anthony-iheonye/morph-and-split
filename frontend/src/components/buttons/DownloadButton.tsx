a86f84a4ef4e07ba6d4cedbd210ffa90e359340f
anthony-iheonye <iheonyeac@gmail.com>
2025-01-16 17:36:03 -0500

    frontend: Update button for  downloading augmented result.

--- frontend/src/components/buttons/DownloadButton.tsx
+++ frontend/src/components/buttons/DownloadButton.tsx
@@ -1,26 +1,25 @@
-import { Button } from "@chakra-ui/react";
+import { IconButton, Tooltip } from "@chakra-ui/react";
 import { FaDownload } from "react-icons/fa6";
 
 interface DownloadFileProps {
   filename: string;
-  label: string;
   onDownload: (filename: string) => void;
 }
 
-const DownloadButton = ({
-  filename,
-  onDownload,
-  label = "Download File",
-}: DownloadFileProps) => {
+const DownloadButton = ({ filename, onDownload }: DownloadFileProps) => {
   return (
-    <Button
-      type="button"
-      colorScheme="teal"
-      leftIcon={<FaDownload />}
-      onClick={() => onDownload(filename)}
-    >
-      {label}
-    </Button>
+    <Tooltip label="Download augmented data">
+      <IconButton
+        aria-label="Click to download augmented result."
+        type="button"
+        color="teal"
+        backgroundColor="transparent"
+        onClick={() => onDownload(filename)}
+        icon={<FaDownload />}
+        borderRadius={20}
+        size={"lg"}
+      />
+    </Tooltip>
   );
 };
 
