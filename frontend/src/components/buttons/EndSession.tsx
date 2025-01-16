18569c1c6330ea50388685b40eec0fecb62041b4
anthony-iheonye <iheonyeac@gmail.com>
2025-01-16 17:34:52 -0500

    frontend: Build "end session" button

--- frontend/src/components/buttons/EndSession.tsx
+++ frontend/src/components/buttons/EndSession.tsx
@@ -1,5 +1,8 @@
 import { Button, useBreakpointValue } from "@chakra-ui/react";
 import { AiOutlineCloseSquare } from "react-icons/ai";
+import { APIClient } from "../../services";
+import { BackendResponse } from "../../entities";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   label?: string | { base?: string; md?: string; lg?: string };
@@ -11,6 +14,21 @@ const EndSession = ({ label = "End session", disable = false }: Props) => {
     typeof label === "string" ? { base: label } : label
   );
 
+  const sessionClient = new APIClient<BackendResponse>("/delete_session");
+  const navigate = useNavigate();
+
+  const handleClick = async () => {
+    try {
+      const response = await sessionClient.endSession();
+
+      if (response.success) {
+        navigate("/");
+      }
+    } catch (err) {
+      console.error(`Error: ${err}`);
+    }
+  };
+
   return (
     <Button
       colorScheme="red"
@@ -18,6 +36,7 @@ const EndSession = ({ label = "End session", disable = false }: Props) => {
       borderRadius={20}
       leftIcon={<AiOutlineCloseSquare />}
       isDisabled={disable}
+      onClick={() => handleClick()}
     >
       {responsiveLabel}
     </Button>
