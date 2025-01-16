6282efc181e49a6653ae36f3bd53517f68c42d82
anthony-iheonye <iheonyeac@gmail.com>
2025-01-16 17:34:21 -0500

    frontend: Create welcome page and start session button.

---
+++ frontend/src/components/buttons/StartSession.tsx
@@ -0,0 +1,49 @@
+import { Button, useBreakpointValue } from "@chakra-ui/react";
+import { IoIosArrowForward } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+import { APIClient } from "../../services";
+import { BackendResponse } from "../../entities";
+
+interface Props {
+  label?: string | { base?: string; md?: string; lg?: string };
+  to: string;
+  disable?: boolean;
+}
+
+const StartSession = ({
+  to,
+  label = "Start a Session",
+  disable = false,
+}: Props) => {
+  const navigate = useNavigate();
+  const storageClient = new APIClient<BackendResponse>("/create_storage");
+
+  const responsiveLabel = useBreakpointValue(
+    typeof label === "string" ? { base: label } : label
+  );
+
+  const handleClick = async () => {
+    try {
+      const response = await storageClient.createStorage();
+
+      if (response.success) {
+        navigate(to);
+      }
+    } catch (err) {}
+  };
+
+  return (
+    <Button
+      colorScheme="teal"
+      size="sm"
+      onClick={() => handleClick()}
+      borderRadius={20}
+      rightIcon={<IoIosArrowForward />}
+      isDisabled={disable}
+    >
+      {responsiveLabel}
+    </Button>
+  );
+};
+
+export default StartSession;
---
+++ frontend/src/pages/WelcomePage.tsx
@@ -0,0 +1,7 @@
+import StartSession from "../components/buttons/StartSession";
+
+const WelcomePage = () => {
+  return <StartSession to="/upload_data/images" label="Start New Session" />;
+};
+
+export default WelcomePage;
