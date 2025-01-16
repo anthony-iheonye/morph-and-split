16c5d0b2968616848e9b41ca8e26c19201b8a897
anthony-iheonye <iheonyeac@gmail.com>
2025-01-16 17:47:22 -0500

    frontend: Update hook for fetching backend response.

--- frontend/src/hooks/useBackendResponse.ts
+++ frontend/src/hooks/useBackendResponse.ts
@@ -1,13 +1,30 @@
 import { useBackendResponseStore } from "../store";
 
 const useBackendResponse = () => {
-  const { augmentationIsComplete, setBackendResponseLog } =
-    useBackendResponseStore((store) => ({
-      setBackendResponseLog: store.setBackendResponseLog,
-      augmentationIsComplete: store.backendResponseLog.augmentationIsComplete,
-    }));
+  const {
+    augmentationIsComplete,
+    backendResponseLog,
+    imageUploaded,
+    maskUploaded,
+    imageMaskCountIsEqual,
+    setBackendResponseLog,
+  } = useBackendResponseStore((store) => ({
+    setBackendResponseLog: store.setBackendResponseLog,
+    augmentationIsComplete: store.backendResponseLog.augmentationIsComplete,
+    imageUploaded: store.backendResponseLog.imageUploaded,
+    maskUploaded: store.backendResponseLog.maskUploaded,
+    imageMaskCountIsEqual: store.backendResponseLog.imageMaskCountIsEqual,
+    backendResponseLog: store.backendResponseLog,
+  }));
 
-  return { augmentationIsComplete, setBackendResponseLog };
+  return {
+    augmentationIsComplete,
+    backendResponseLog,
+    imageUploaded,
+    maskUploaded,
+    imageMaskCountIsEqual,
+    setBackendResponseLog,
+  };
 };
 
 export default useBackendResponse;
