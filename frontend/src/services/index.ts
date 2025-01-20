4e83304a6ea1f0379ff8dd55572738877ada008c
anthony-iheonye <iheonyeac@gmail.com>
2025-01-19 19:51:21 -0500

    frontend: Create function that handles the clicking of links.

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,4 +1,5 @@
 export { default as APIClient } from "./api-client";
+export { default as handleLinkClick } from "./handleLinkClick";
 export { default as handleEndSession } from "./handleEndSession";
 export { default as sizes } from "./size";
 export { default as sortByName } from "./sortByName";
