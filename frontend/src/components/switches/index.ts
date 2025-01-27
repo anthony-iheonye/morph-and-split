182f7567e65b5c7ef849922423eb420c756d8bc9
anthony-iheonye <iheonyeac@gmail.com>
2025-01-27 12:48:31 -0500

    frontend: Build the VisualAttributeSwitch Component.

--- frontend/src/components/switches/index.ts
+++ frontend/src/components/switches/index.ts
@@ -4,3 +4,4 @@ export { default as RandomTransformation } from "./RandomTransformation";
 export { default as TestRatioLock } from "./TestRatioLock";
 export { default as TrainRatioLock } from "./TrainRatioLock";
 export { default as ValRatioLock } from "./ValRatioLock";
+export { default as VisualAttributeSwitch } from "./VisualAttributeSwitch";
