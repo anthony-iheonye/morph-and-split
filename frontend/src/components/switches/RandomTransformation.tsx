2e686d8ea19d328f9b1eaca4149ea68618aec33c
anthony-iheonye <iheonyeac@gmail.com>
2025-02-16 12:48:17 -0500

    frontend: Update RandomTransformation component.

--- frontend/src/components/switches/RandomTransformation.tsx
+++ frontend/src/components/switches/RandomTransformation.tsx
@@ -3,8 +3,8 @@ import { IconType } from "react-icons";
 import { useAugConfigAndSetter } from "../../hooks";
 import { IconComboControl } from "../miscellaneous";
 
-interface Props extends GridProps {
-  title: string;
+interface Props extends Omit<GridProps, "title"> {
+  title: string | { base?: string; md?: string; lg?: string };
   titleFontSize?: number | { base?: number; md?: number; lg?: number };
   description?: string | { base?: string; md?: string; lg?: string };
   transformName:
@@ -33,6 +33,10 @@ const RandomTransformation = ({
     typeof description === "string" ? { base: description } : description
   );
 
+  const responsiveTitle =
+    useBreakpointValue(typeof title === "string" ? { base: title } : title) ||
+    "";
+
   const handleCheckBoxChange = (key: keyof typeof augConfig) => {
     setAugConfig(key, !augConfig[key]);
   };
@@ -53,13 +57,13 @@ const RandomTransformation = ({
     corruptSaturation:
       "Alters the intensity of colors to simulate different saturation levels.",
     augmentValData:
-      "Apply the selected random transformations to the validation set.",
+      "Apply selected transformations to validation and test sets.",
   };
 
   return (
     <IconComboControl
       icon={icon}
-      title={title}
+      title={responsiveTitle}
       description={transforms[transformName] || responsiveDescription}
       titleFontSize={titleFontSize}
       controlElement={
