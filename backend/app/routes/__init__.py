00f0f529f99115ec6af42b658385265ff417f20c
anthony-iheonye <iheonyeac@gmail.com>
2025-02-21 18:53:18 -0500

    backend: Create route to transfer resized original data to GCS bucket

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -2,13 +2,14 @@
 from .augment import augment
 from .directory_management import directory_management
 from .download_file import download_file
-from .gcs_data_transfer import download_data_from_gcs
+from .gcs_data_transfer import download_data_from_gcs, transfer_data_to_gcs
 from .gcs_managment import gcs_management
 from .image_mask_metadata import image_mask_metadata
 from .image_upload import image_upload
 from .mask_upload import mask_upload
 from .reset_session import reset_session
 from .resize_data import resize_data
-from .signed_upload_url import signed_upload_url
+from .signed_upload_urls import signed_upload_urls
+from .signed_download_urls import signed_download_urls
 from .status_check import status_checks
 from .uploaded_file_names import uploaded_file_names
--- backend/app/routes/gcs_data_transfer.py
+++ backend/app/routes/gcs_data_transfer.py
@@ -1,13 +1,106 @@
 import gc
+import os.path
 
 from flask import Blueprint, jsonify
 from tensorflow.keras.backend import clear_session
 
 from app.config import google_cloud_config
-from app.utils import download_files_from_gcs_folder, directory_store
+from app.utils import download_files_from_gcs_folder, directory_store, upload_file_to_gcs_bucket, \
+    upload_files_to_gcs_bucket
+
+transfer_data_to_gcs = Blueprint(name='transfer_data_to_gcs', import_name=__name__)
 
 download_data_from_gcs = Blueprint(name='download_data_from_gcs',
                                    import_name=__name__)
+
+AUGMENTED_DIR = directory_store.augmented
+
+
+@transfer_data_to_gcs.route('/gcs/transfer_augmented_zip_to_gcs', methods=['POST'])
+def transfer_augmented_zip_to_gcs():
+    """uploads the zip file containing augmented results to Google Cloud storage bucket """
+    # filename = secure_filename(filename)
+
+    filename = 'augmented_data.zip'
+
+    try:
+        if os.path.exists(os.path.join(AUGMENTED_DIR, filename)):
+            destination_blob_name = google_cloud_config.augmented_dir + '/' + filename
+            source_file_name = os.path.join(AUGMENTED_DIR, filename)
+
+            upload_file_to_gcs_bucket(bucket_name=google_cloud_config.bucket_name,
+                                      source_file_name= source_file_name,
+                                      destination_blob_name=destination_blob_name)
+
+            return jsonify({'success': True,
+                            'message': f"{source_file_name} uploaded to GCS bucket as {destination_blob_name}"}), 200
+
+        else:
+            return jsonify({'success': False,
+                            'message': f"{filename} was not found"}), 200
+
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)}), 500
+
+
+@transfer_data_to_gcs.route('/gcs/transfer_resized_augmented_data_to_gcs', methods=['POST'])
+def transfer_resized_augmented_data():
+    """
+    Transfer the resized augmented images and masks to Google Cloud bucket
+    """
+    try:
+        upload_files_to_gcs_bucket(bucket_name=google_cloud_config.bucket_name,
+                                   source_folder_path=directory_store.resized_augmented,
+                                   destination_folder_path=google_cloud_config.resized_augmented,
+                                   copy=False)
+
+        return jsonify({'success': True, 'message': "Successfully transferred resized original masks to bucket."}), 200
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)}), 500
+
+    finally:
+        clear_session()
+        gc.collect()
+
+
+@transfer_data_to_gcs.route('/gcs/transfer_resized_original_images_to_gcs', methods=['POST'])
+def transfer_resized_original_images():
+    """
+    Transfer the resized original images and mask to Google Cloud bucket
+    """
+    try:
+        upload_files_to_gcs_bucket(bucket_name=google_cloud_config.bucket_name,
+                                   source_folder_path=directory_store.resized_image_dir,
+                                   destination_folder_path=google_cloud_config.resized_image_dir,
+                                   copy=True)
+
+        return jsonify({'success': True, 'message': "Successfully transferred resized original images to bucket."}), 200
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)}), 500
+
+    finally:
+        clear_session()
+        gc.collect()
+
+
+@transfer_data_to_gcs.route('/gcs/transfer_resized_original_masks_to_gcs', methods=['POST'])
+def transfer_resized_original_masks():
+    """
+    Transfer the resized original images and mask to Google Cloud bucket
+    """
+    try:
+        upload_files_to_gcs_bucket(bucket_name=google_cloud_config.bucket_name,
+                                   source_folder_path=directory_store.resized_mask_dir,
+                                   destination_folder_path=google_cloud_config.resized_mask_dir,
+                                   copy=False)
+
+        return jsonify({'success': True, 'message': "Successfully transferred resized original masks to bucket."}), 200
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)}), 500
+
+    finally:
+        clear_session()
+        gc.collect()
 
 
 @download_data_from_gcs.route('/gcs/transfer_images_to_backend', methods=['POST'])
