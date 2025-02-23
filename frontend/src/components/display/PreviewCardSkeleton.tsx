f0b1ae00e24730c9dc3cfea6d22ddb25cd406b42
anthony-iheonye <iheonyeac@gmail.com>
2025-02-23 06:43:31 -0500

    frontend: Update preview skeleton

--- frontend/src/components/display/PreviewCardSkeleton.tsx
+++ frontend/src/components/display/PreviewCardSkeleton.tsx
@@ -1,27 +1,13 @@
-import {
-  Card,
-  CardBody,
-  HStack,
-  Skeleton,
-  SkeletonText,
-} from "@chakra-ui/react";
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const PreviewCardSkeleton = () => {
   return (
-    <HStack spacing={0}>
-      <Card>
-        <Skeleton height="200px" />
-        <CardBody>
-          <SkeletonText />
-        </CardBody>
-      </Card>
-      <Card>
-        <Skeleton height="200px" />
-        <CardBody>
-          <SkeletonText />
-        </CardBody>
-      </Card>
-    </HStack>
+    <Card>
+      <Skeleton height="170px" />
+      <CardBody>
+        <SkeletonText fontSize="sm" />
+      </CardBody>
+    </Card>
   );
 };
 
