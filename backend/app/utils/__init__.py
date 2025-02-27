6fdf455d1e8add3187fabfd096ce1603fc469983
anthony-iheonye <iheonyeac@gmail.com>
2025-02-27 18:40:21 -0500

    backend: Create ValidationError class

--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -3,9 +3,12 @@ from .directory_file_management import (create_directory,
                                         get_sorted_filenames,
                                         current_directory,
                                         list_filenames,
-                                        directory_exit)
+                                        directory_exit,
+                                        delete_file)
 
 from .directory_file_management import (create_project_directories,
                                         get_sorted_filepaths,
                                         create_resized_augmentation_directories,
                                         delete_directory)
+
+from .errors import ValidationError
