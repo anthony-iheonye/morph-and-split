49294aa4214e8e03cd0fbd61932f6fb8dde27114
anthony-iheonye <iheonyeac@gmail.com>
2025-05-21 17:32:23 -0400

    Backend: Update GoogleCloudStorage Config

--- backend/app/config/google_cloud_storage.py
+++ backend/app/config/google_cloud_storage.py
@@ -9,16 +9,19 @@ from app.utils import current_directory
 @attr.s
 class GoogleCloudStorageConfig:
     # Google Cloud Project
-    project_name: str = attr.ib(default='morph-and-split-toolkit')
+    project_name: str = attr.ib(default='morph-and-split-engine')
 
     # Service Account
-    service_account_name: str = attr.ib(default='morph-and-split-toolkit-sa')
-    service_account_key_file_name: str = attr.ib(default='morph-and-split-toolkit-key.json')
-    service_account_key_file_path: Optional[str] = attr.ib(init=False, default=None)
+    service_account_name: str = attr.ib(default='morph-and-split-engine-sa')
+    service_account_key_file_name: str = attr.ib(
+        default='morph-and-split-engine-key.json')
+    service_account_key_file_path: Optional[str] = attr.ib(
+        init=False, default=None)
     service_account_email: str = attr.ib(init=False, default=None)
 
     # Details for impersonated credentials.source
-    target_scopes: list = attr.ib(factory=lambda: ['https://www.googleapis.com/auth/cloud-platform'])
+    target_scopes: list = attr.ib(
+        factory=lambda: ['https://www.googleapis.com/auth/cloud-platform'])
 
     # Google Cloud Storage Bucket
     location: str = attr.ib(default='US-SOUTH1')
@@ -26,7 +29,8 @@ class GoogleCloudStorageConfig:
     enable_uniform_bucket_level_access: bool = attr.ib(default=True)
 
     origin: List[str] = attr.ib(factory=lambda: ["*"])
-    responseHeader: List[str] = attr.ib(factory=lambda: ["Content-Type", "x-goog-content-resumable"])
+    responseHeader: List[str] = attr.ib(
+        factory=lambda: ["Content-Type", "x-goog-content-resumable"])
     method: List[str] = attr.ib(factory=lambda: ['PUT', 'POST', 'GET'])
     maxAgeSeconds: int = attr.ib(default=3600)
     cors: List[Dict] = attr.ib(factory=lambda: [{
@@ -41,17 +45,24 @@ class GoogleCloudStorageConfig:
     resized_image_dir: str = attr.ib(default='resized_images')
     resized_mask_dir: str = attr.ib(default='resized_masks')
     resized_augmented: str = attr.ib(default='resized_augmented')
-    resized_train_images_dir: str = attr.ib(default='resized_augmented/train/images')
-    resized_train_masks_dir: str = attr.ib(default='resized_augmented/train/masks')
-    resized_val_images_dir: str = attr.ib(default='resized_augmented/val/images')
+    resized_train_images_dir: str = attr.ib(
+        default='resized_augmented/train/images')
+    resized_train_masks_dir: str = attr.ib(
+        default='resized_augmented/train/masks')
+    resized_val_images_dir: str = attr.ib(
+        default='resized_augmented/val/images')
     resized_val_masks_dir: str = attr.ib(default='resized_augmented/val/masks')
-    resized_test_images_dir: str = attr.ib(default='resized_augmented/test/images')
-    resized_test_masks_dir: str = attr.ib(default='resized_augmented/test/masks')
+    resized_test_images_dir: str = attr.ib(
+        default='resized_augmented/test/images')
+    resized_test_masks_dir: str = attr.ib(
+        default='resized_augmented/test/masks')
     augmented_dir: str = attr.ib(default='augmented/combined')
 
     def __attrs_post_init__(self):
-        self.service_account_key_file_path =  str(os.path.join(current_directory(), self.service_account_key_file_name))
+        self.service_account_key_file_path = str(os.path.join(
+            current_directory(), self.service_account_key_file_name))
         self.service_account_email = f"{self.service_account_name}@{self.project_name}.iam.gserviceaccount.com"
+
     @property
     def gcs_directories(self):
         return [self.image_dir,
@@ -71,4 +82,3 @@ class GoogleCloudStorageConfig:
     @property
     def gcs_cors(self):
         return self.cors
-
